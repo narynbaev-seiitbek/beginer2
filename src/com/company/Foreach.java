8934a6ce4ae3b0d77aceda351f7cd03847d2701a
User <narynbaev.seiitbek@gmail.com>
2022-01-26 21:09:31 +0300

    b huk hbu

---
+++ src/com/company/Foreach.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.lang.invoke.StringConcatFactory;
+import java.util.Scanner;
+
+public class Foreach {
+      static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+            string(scanner.nextLine());
+    }
+    static void string(String str) {
+
+        String str2 = str.toUpperCase();
+        String str3 = str2.trim();
+        String str4 = str3.contains(scanner.nextLine("a"));
+        System.out.println(scanner.nextLine().toUpperCase().toUpperCase().trim().contains("a"));
+
+    }
+
+}
